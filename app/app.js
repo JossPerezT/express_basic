b893fc7966816e851dd3161cd2ff421efab7a4ca
JossPerezT <jossperez44@gmail.com>
2022-04-29 23:59:44 -0500

    5. Agregando una nueva ruta que reciba los valores Query Params y viendo como funciona

--- app/app.js
+++ app/app.js
@@ -19,6 +19,11 @@ app.get ('/explorersInNode', (req, res) =>{
     res.send(explorer)
 })
 
+app.get ('/explorers/:explorerName', (req, res) =>{
+   
+    res.send (req.params)
+})
+
 app.listen(port, () => {
     console.log (`Example app listening on port ${port}`)
 })
