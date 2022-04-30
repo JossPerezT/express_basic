ae29ff245e69a4c313472fcc80977dcb175c8318
JossPerezT <jossperez44@gmail.com>
2022-04-29 23:25:55 -0500

    3. Agregando una nueva ruta a nuestro proyecto en express

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@ app.get ('/', (req, res) => {
     res.send('Hello World!!')
 })
 
+app.get ('/launchx', (req, res) =>{
+    res.send(`Bienvenidos a LaunchX`)
+})
 
 app.listen(port, () => {
     console.log (`Example app listening on port ${port}`)
