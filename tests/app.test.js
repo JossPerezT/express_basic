9b63f2afe2ee2907ce7eb733c4084945af37e915
JossPerezT <jossperez44@gmail.com>
2022-04-30 19:58:02 -0500

    10. Agregando la prueba básica para agregar una nueva ruta

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,8 +3,13 @@ const request = require('supertest')
 
 describe ('Tests para express server', ()=>{
     it('Primera entrada: Hello World', async ()=>{
-       const res = await request(app).get('/launchx').send(`Bienvenidos a LaunchX`)
+       const res = await request(app).get('/').send(`Hello World!!`)
 
-        expect(res.statusCode).toEqual(200)
+        expect(res.text).toBe(`Hello World!!`)
+    })
+    it ('Agregar una nueva ruta', async ()=>{
+        const res = await request(app).get('/launchx').send(`Bienvenidos a LaunchX`)
+
+        expect(res.text).toMatch('Bienvenidos a LaunchX')
     })
 })
