266677d7d25fe659717a539f2362a849dfa267f4
JossPerezT <jossperez44@gmail.com>
2022-04-30 20:53:13 -0500

    11. Agregando las pruebas de unidad de 3 de los ejercicios de Mi primer servidor con Express de la semana 3

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,12 +4,22 @@ const request = require('supertest')
 describe ('Tests para express server', ()=>{
     it('Primera entrada: Hello World', async ()=>{
        const res = await request(app).get('/').send(`Hello World!!`)
-
-        expect(res.text).toBe(`Hello World!!`)
+       expect((res)=>{
+           expect(res.text).toBe(`Hello World!!`)
+        })
     })
     it ('Agregar una nueva ruta', async ()=>{
         const res = await request(app).get('/launchx').send(`Bienvenidos a LaunchX`)
-
-        expect(res.text).toMatch('Bienvenidos a LaunchX')
+        expect((res)=>{
+            expect(res.text).toMatch('Bienvenidos a LaunchX')
+        })
+    })
+    it ('agregar una nueva ruta y que regrese un objeto', () =>{
+        const explorer = {name: "Explorer1", msg: "Hello!"}
+        const res = request(app).get('/explorerInNode').send(explorer)
+        expect((res)=>{
+            expect((res.body).name).toBe("Explorer1")
+            expect((res.body).msg).toBe("Hello!")
+        })
     })
 })
